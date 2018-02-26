c78f0000473be7a67685297bf339b6db2b6072ad
Corentin Le Molgat <corentinl@google.com>
2018-02-26 17:43:50 +0100

    remove redundant file

--- makefiles/Makefile.test.mk
+++ makefiles/Makefile.test.mk
@@ -47,7 +47,6 @@ test_csharp_examples: \
 	$(MONO) $(BIN_DIR)$Scscvrptw$(CLR_EXE_SUFFIX).exe
 	$(MONO) $(BIN_DIR)$Stestlp$(CLR_EXE_SUFFIX).exe
 	$(MONO) $(BIN_DIR)$Stestcp$(CLR_EXE_SUFFIX).exe
-	$(MONO) $(BIN_DIR)$Stestcp$(CLR_EXE_SUFFIX).exe
 	$(MONO) $(BIN_DIR)$Stest_sat_model$(CLR_EXE_SUFFIX).exe
 
 test_fsharp_examples: fsharp
